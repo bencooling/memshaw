1294ee1623a4c5834c48b081baa40fac411c793e
Ben Cooling <office@bcooling.com.au>
2016-03-11 13:42:34 +1000

    compiling es6

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
-import fs from 'fs';
-import path from 'path';
-import sio from 'socket.io';
-import bytes from 'bytes';
+'use strict';
 
-const getMemory = () => {
-  const m = process.memoryUsage();
-  const data = Object.keys(m).reduce((o, key) => {
-    o[key] = Number(bytes(m[key]).replace('MB', ''));
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _socket = require('socket.io');
+
+var _socket2 = _interopRequireDefault(_socket);
+
+var _bytes = require('bytes');
+
+var _bytes2 = _interopRequireDefault(_bytes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var getMemory = function getMemory() {
+  var m = process.memoryUsage();
+  var data = Object.keys(m).reduce(function (o, key) {
+    o[key] = Number((0, _bytes2.default)(m[key]).replace('MB', ''));
     return o;
   }, {});
-  data.time = (Date.now() / 1000);
+  data.time = Date.now() / 1000;
   return data;
 };
-const compileTpl = (s, d) =>
-  s.replace(/{{(.+?)}}/g, (match, p1) => (d[p1]) ? d[p1] : '');
+var compileTpl = function compileTpl(s, d) {
+  return s.replace(/{{(.+?)}}/g, function (match, p1) {
+    return d[p1] ? d[p1] : '';
+  });
+};
 
-exports.register = (server, options, next) => {
+exports.register = function (server, options, next) {
   server.route({
     method: 'GET',
     path: '/memshaw',
-    handler: (request, reply) => {
-      const io = sio(server.listener);
-      io.on('connection', (socket) => {
+    handler: function handler(request, reply) {
+      var io = (0, _socket2.default)(server.listener);
+      io.on('connection', function (socket) {
         server.log(['socket'], 'user connected');
-        socket.on('disconnect', () => {
+        socket.on('disconnect', function () {
           server.log(['socket'], 'user disconnected');
         });
-        setInterval(() => {
+        setInterval(function () {
           io.emit('memory usage', getMemory());
         }, 1000);
       });
       // Simple string replace templating
-      reply(compileTpl(
-        fs.readFileSync(path.join(__dirname, './index.html'), 'utf8'),
-        getMemory()));
-    },
+      reply(compileTpl(_fs2.default.readFileSync(_path2.default.join(__dirname, './index.html'), 'utf8'), getMemory()));
+    }
   });
 
   next();
@@ -42,5 +58,5 @@ exports.register = (server, options, next) => {
 
 exports.register.attributes = {
   name: 'memshaw',
-  version: '0.0.1',
+  version: '0.1.3'
 };
